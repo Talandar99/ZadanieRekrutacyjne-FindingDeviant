fddefd8468739ddbf8098c5a5e05cebd485e3d59
Talandar99 <grzegorzc66@gmail.com>
2022-06-03 17:40:45 +0200

    dobła przerwa

--- lib/model.dart
+++ lib/model.dart
@@ -1,26 +1,28 @@
+///class is responsible for creating and manipulating array
+class ArrayManipulator {
 
+  ///Method get array of numbers and return deviant number.
+  ///Deviant is even number in odd numbers list,
+  /// or odd number in even numbers list.
+  int returnDeviantNumber(List<int> numbers) {
+    List<int> oddNumbers = [];
+    List<int> evenNumbers = [];
 
+    for (var number in numbers) {
+      if (number % 2 == 0) {
+        oddNumbers.add(number);
+      } else {
+        evenNumbers.add(number);
+      }
+    }
 
-
-
-///Method get array of numbers and return deviant number.
-///Deviant is even number in odd numbers list,
-/// or odd number in even numbers list.
-int returnDeviantNumber(List<int> numbers) {
-  List<int> oddNumbers = [];
-  List<int> evenNumbers = [];
-
-  for (var number in numbers) {
-    if (number % 2 == 0) {
-      oddNumbers.add(number);
+    if (oddNumbers.length < evenNumbers.length) {
+      return oddNumbers.first;
     } else {
-      evenNumbers.add(number);
+      return evenNumbers.first;
     }
   }
-
-  if (oddNumbers.length < evenNumbers.length) {
-    return oddNumbers.first;
-  } else {
-    return evenNumbers.first;
-  }
 }
+
+class viewModel{}
+//view model to to co się wy  
