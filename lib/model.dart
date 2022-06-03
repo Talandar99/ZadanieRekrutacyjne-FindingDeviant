f18a0a49cc3cabfe4f239c90c61522f9639ec24b
Talandar99 <grzegorzc66@gmail.com>
2022-06-03 17:26:47 +0200

    init

---
+++ lib/model.dart
@@ -0,0 +1,26 @@
+
+
+
+
+
+///Method get array of numbers and return deviant number.
+///Deviant is even number in odd numbers list,
+/// or odd number in even numbers list.
+int returnDeviantNumber(List<int> numbers) {
+  List<int> oddNumbers = [];
+  List<int> evenNumbers = [];
+
+  for (var number in numbers) {
+    if (number % 2 == 0) {
+      oddNumbers.add(number);
+    } else {
+      evenNumbers.add(number);
+    }
+  }
+
+  if (oddNumbers.length < evenNumbers.length) {
+    return oddNumbers.first;
+  } else {
+    return evenNumbers.first;
+  }
+}
